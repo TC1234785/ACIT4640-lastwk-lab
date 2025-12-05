686008278318325114411ff92c59e4cc3f7a45bb
andrei mendoza <amendoza39@my.bcit.ca>
2025-12-05 10:00:13 -0800

    fixes stuff

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,8 @@
-# configure version of aws provider plugin
-# https://developer.hashicorp.com/terraform/language/terraform#terraform
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "~> 6.21.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
